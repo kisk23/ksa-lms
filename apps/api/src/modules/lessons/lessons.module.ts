4a3739b9a48a64c2249e67c4c6c0f5ffa9283307
Mohamed Tarek Mohamed <mohamedtar425@gmail.com>
2026-06-03 17:14:38 +0300

    fix(lessons): fixed dependency issue in LessonsModule

--- apps/api/src/modules/lessons/lessons.module.ts
+++ apps/api/src/modules/lessons/lessons.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { CoursesModule } from '../courses/courses.module';
 import { LessonAccessGuard } from '../courses/guards/lesson-access.guard';
 import { LessonsService } from '../courses/lesson.service';
 
 @Module({
+  imports: [CoursesModule],
   providers: [LessonsService, LessonAccessGuard],
   exports: [LessonsService, LessonAccessGuard],
 })
