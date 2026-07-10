9ea1982ee60a8441cbd0e0c1dcbb59cd99431fb1
Mohamed Tarek Mohamed <mohamedtar425@gmail.com>
2026-07-10 16:47:45 +0300

    fix: Resolved Prisma Client Generation & Build Errors - created teachers route

---
+++ apps/web/src/app/teachers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function TeachersPage() {
+  return (
+    <div>
+        <h1 className='text-3xl font-bold text-primary'>
+            Teachers Page
+        </h1>
+    </div>
+  )
+}
